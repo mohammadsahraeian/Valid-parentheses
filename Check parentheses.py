4f8bba82f61878735fb4414519c7f1fb221c88b2
mohammadsahraeian <88543539+mohammadsahraeian@users.noreply.github.com>
2021-11-12 23:17:13 +0330

    Add files via upload
    By this code, you can determined whether your parentheses written correctly or not.

---
+++ Check parentheses.py
@@ -0,0 +1,25 @@
+open_list = ["(", "[", "{"]
+close_list = [")", "]", "}"]
+
+def check(string):
+    stack = []
+    for sign in string:
+        if sign in open_list:
+            stack.append(sign)
+        elif sign in close_list:
+            close_index = close_list.index(sign)
+            if len(stack) > 0 and (open_list[close_index] == stack[len(stack)-1]):
+                stack.pop()
+            else:
+                return "Unbalance"
+    if len(stack) == 0:
+        return "Balance"
+    else:
+        return "Unbalance"
+
+first = '({([])}{[}])'
+print(check(first))
+second = '{({)}}'
+print(check(second))
+third = '[{([()])}]'
+print(check(third))
